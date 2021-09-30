b365cf62196da67ad20751a968970e2747436996
Abdullah Al Adwani <abdullahmmaladwani@gmail.com>
2021-09-30 17:13:14 +0300

    Part 3 Done

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ tasks = [
   { id: 0, title: "Game of thrones", category: "Movies", done: false },
   { id: 1, title: "Toy Story 4", category: "Movies", done: false },
   { id: 2, title: "Toy Story 3", category: "Movies", done: true },
-
 ];
 
 categories = ["Movies", "Groceries"];
@@ -19,25 +18,29 @@ renderCategories(categories, CATEGORY_SELECTOR);
 renderCategories(categories, CATEGORY_FILTER);
 renderTasks(tasks, "tasks-list");
 
-
 function taskChecked(taskId, checked) {
-  // implement the delete task.
-  // You are given the task id
-  console.log(`${checked ? "" : "UN"}CHECKED TASK`, taskId);
+  tasks[taskId].done = checked;
+  renderTasks(tasks, "tasks-list");
 }
 
 function addTask() {
   const selectedCategory = getSelectedCategoryById(CATEGORY_SELECTOR);
   const taskTitle = getNewTaskText();
-  tasks.push({id:(tasks.length), title: taskTitle, category: selectedCategory, done: false })
+  tasks.push({
+    id: tasks.length,
+    title: taskTitle,
+    category: selectedCategory,
+    done: false,
+  });
   renderTasks(tasks, "tasks-list");
-
-  
 }
 
 function addCategory() {
   const newCategory = getNewCategoryText();
-  // continue the code here
+  categories.push(newCategory);
+  renderCategories(categories, CATEGORY_SELECTOR);
+  renderCategories(categories, CATEGORY_FILTER);
+
   alert(`New category was added: ${newCategory}`);
 }
 
